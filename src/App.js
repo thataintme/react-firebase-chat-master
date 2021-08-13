08126b9f02aa77c1592d5c4858cdd0b3f717e6c6
thataintme <47311803+thataintme@users.noreply.github.com>
2021-08-13 19:18:31 +0530

    Removed the Firebase creds

--- src/App.js
+++ src/App.js
@@ -13,13 +13,7 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 import useLocalStorage from './hooks/useLocaStorage';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyA3hAImaUfe30ISl0z5PMxjw9SloKT8KTE",
-  authDomain: "superchat-f279e.firebaseapp.com",
-  projectId: "superchat-f279e",
-  storageBucket: "superchat-f279e.appspot.com",
-  messagingSenderId: "21649455285",
-  appId: "1:21649455285:web:d0d4e68770137cc77226fc",
-  measurementId: "G-C439PF8EQF"
+  //YOUR FIREBASE CREDENTIALS HERE
 })
 
 const auth = firebase.auth();
